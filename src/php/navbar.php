ef83aabb4b218698a38c1d9601234f321c3a4ccc
Christina Osipova <chosipova@gmail.com>
2019-03-21 11:30:13 +0300

    много всего

---
+++ src/php/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar">
+    <div class="container">
+      <div class="navbar-block">
+        <div class="navbar__logo">
+          <img src="img/navbar/balaclava-logo.png" alt="logo" class="logo">
+        </div>
+        <div class="navbar-menu">
+          <span class="navbar-menu__btn" id="menuBtn">
+            <a href="#"><img src="img/navbar/menu.svg" alt="burger-menu" id="btn__image"></a>
+          </span>
+          <ul class="menu navbar-menu__list" id="menuList">
+            <li class="menu__item navbar-menu__item"> <a href="#">О нас</a></li>
+            <li class="menu__item navbar-menu__item"><a href="#">Наши номера</a></li>
+            <li class="menu__item navbar-menu__item"><a href="#">Русская баня</a></li>
+            <li class="menu__item navbar-menu__item"><a href="#">Достопримечательности</a></li>
+            <li class="menu__item navbar-menu__item"><a href="#">Контакты</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
